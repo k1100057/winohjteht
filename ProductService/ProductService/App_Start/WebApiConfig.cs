6b381b3820bef6c25c58745680b91d3d88e66438
ari.luoma3@seamk.fi <ari.luoma3@seamk.fi>
2015-03-19 18:49:18 +0200

    Eka commit

---
+++ ProductService/ProductService/App_Start/WebApiConfig.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using ProductService.Models;
+using System.Web.OData.Builder;
+using System.Web.OData.Extensions;
+
+namespace ProductService
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            ODataModelBuilder builder = new ODataConventionModelBuilder();
+            builder.EntitySet<Product>("Products");
+            builder.Function("GetSalesTaxRate").Returns<double>().Parameter<int>("PostalCode");
+            builder.Namespace = "ProductService";
+            builder.EntityType<Product>()
+                .Action("Rate")
+                .Parameter<int>("Rating");
+            builder.EntitySet<Supplier>("Suppliers");
+            config.MapODataServiceRoute(
+                routeName: "ODataRoute",
+                routePrefix: null,
+                model: builder.GetEdmModel());
+            builder.Namespace = "ProductService";
+            builder.EntityType<Product>().Collection
+                .Function("MostExpensive")
+                .Returns<double>();
+        }
+    }
+}
---
+++ ProductService/ProductService/Controllers/SuppliersController.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.OData;
+using System.Web.Http;
+using ProductService.Models;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace ProductService.Controllers
+{
+    public class SuppliersController : ODataController
+    {
+        ProductsContext db = new ProductsContext();
+
+        public async Task<IHttpActionResult> DeleteRef([FromODataUri] int key,
+        [FromODataUri] string relatedKey, string navigationProperty)
+        {
+            var supplier = await db.Suppliers.SingleOrDefaultAsync(p => p.Id == key);
+            if (supplier == null)
+            {
+                return StatusCode(HttpStatusCode.NotFound);
+            }
+
+            switch (navigationProperty)
+            {
+                case "Products":
+                    var productId = Convert.ToInt32(relatedKey);
+                    var product = await db.Products.SingleOrDefaultAsync(p => p.Id == productId);
+
+                    if (product == null)
+                    {
+                        return NotFound();
+                    }
+                    product.Supplier = null;
+                    break;
+                default:
+                    return StatusCode(HttpStatusCode.NotImplemented);
+
+            }
+            await db.SaveChangesAsync();
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+
+        private bool SupplierExists(int key)
+        {
+            return db.Suppliers.Any(s => s.Id == key);
+        } 
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+
+        [EnableQuery]
+        public IQueryable<Supplier> Get()
+        {
+            return db.Suppliers;
+        }
+
+        [EnableQuery]
+        public SingleResult<Supplier> Get([FromODataUri] int key)
+        {
+            IQueryable<Supplier> result = db.Suppliers.Where(p => p.Id == key);
+            return SingleResult.Create(result);
+        }
+
+        // GET /Products(1)/Supplier
+        [EnableQuery]
+        public SingleResult<Supplier> GetSupplier([FromODataUri] int key)
+        {
+            var result = db.Products.Where(m => m.Id == key).Select(m => m.Supplier);
+            return SingleResult.Create(result);
+        }
+
+        // GET /Suppliers(1)/Products
+        [EnableQuery]
+        public IQueryable<Product> GetProducts([FromODataUri] int key)
+        {
+            return db.Suppliers.Where(m => m.Id.Equals(key)).SelectMany(m => m.Products);
+        }
+
+
+        public async Task<IHttpActionResult> Post(Supplier supplier)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            db.Suppliers.Add(supplier);
+            await db.SaveChangesAsync();
+            return Created(supplier);
+        }
+
+        public async Task<IHttpActionResult> Patch([FromODataUri] int key, Delta<Supplier> supplier)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var entity = await db.Suppliers.FindAsync(key);
+            if (entity == null)
+            {
+                return NotFound();
+            }
+            supplier.Patch(entity);
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!SupplierExists(key))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return Updated(entity);
+        }
+
+        public async Task<IHttpActionResult> Put([FromODataUri] int key, Supplier update)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            if (key != update.Id)
+            {
+                return BadRequest();
+            }
+            db.Entry(update).State = EntityState.Modified;
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!SupplierExists(key))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return Updated(update);
+        }
+
+        public async Task<IHttpActionResult> Delete([FromODataUri] int key)
+        {
+            var supplier = await db.Suppliers.FindAsync(key);
+            if (supplier == null)
+            {
+                return NotFound();
+            }
+            db.Suppliers.Remove(supplier);
+            await db.SaveChangesAsync();
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+    }
+}
